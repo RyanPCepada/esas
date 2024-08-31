c50e37f9f2693f194a9e5c65874f945624e27735
RyanPCepada <ryancepada1992@gmail.com>
2024-08-31 12:14:56 +0800

    added index.php for esas_moderator

---
+++ esas_moderator/index.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>eSAS - Index</title>
+    <link href="../assets/css/jquery.dataTables.min.css" rel="stylesheet" />
+    <script src="../assets/js/all.js" crossorigin="anonymous"></script>
+    <script src="../assets/js/jquery-3.6.0.js"></script>
+    <link href="../assets/css/styles.css" rel="stylesheet" />
+    <link href="../assets/img/nbsclogo.png" rel="icon">
+</head>
+<body>
+    <script>
+        function to_login() {
+            window.location.href = '/esas/esas_moderator/login.php';
+        }
+
+        to_login();
+    </script>
+
+    
+    <!-- <?php include 'assets/components/modals.php' ?> -->
+    <script src="../assets/js/jquery.dataTables.min.js"></script>
+    <script src="../assets/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/js/global_script.js"></script>
+    <script>
+        // clickSubModule('nav/userreg/userreg_main.php')
+        // clickSubModule('nav/purchaserequest/pr_main.php');
+        // clickSubModule('nav/purchaserequest/pr_main.php')
+    </script>
+</body>
+</html>
