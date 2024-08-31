1ddffe381e6361a0fd9a7fb3221cba3499b3f043
RyanPCepada <ryancepada1992@gmail.com>
2024-08-31 11:51:15 +0800

    first commit - setting up

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+$host = 'localhost';
+$dbname = 'esas';
+$username = 'root';
+$password = '';
+
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'e_sas');
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
