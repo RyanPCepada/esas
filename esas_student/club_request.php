51102050199c9f8dbb7ac38b9d2f1a9589f56baa
RyanPCepada <ryancepada1992@gmail.com>
2024-09-07 12:07:51 +0800

    added href on cancel button in club_request.php

--- esas_student/club_request.php
+++ esas_student/club_request.php
@@ -163,7 +163,7 @@
                                     <img id="coverPhotoPreview" src="#" alt="Cover Photo Preview" style="display:none; width: 50%; object-fit: cover;" />
                                 </div>
                                 
-                                <a href="#" onclick="history.back(); return false;" class="btn btn-secondary float-end">Cancel</a>
+                                <a href="javascript:history.go(-1)" return false;" class="btn btn-secondary float-end">Cancel</a>
                             </form>
 
                             <script>
