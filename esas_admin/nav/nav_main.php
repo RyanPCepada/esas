873184668f18f1249b23d2425063e67beb2687cb
RyanPCepada <ryancepada1992@gmail.com>
2024-10-14 19:50:30 +0800

    added history.php in admin folder

--- esas_admin/nav/nav_main.php
+++ esas_admin/nav/nav_main.php
@@ -55,6 +55,8 @@
             <li>
                 <hr class="dropdown-divider" />
             </li>
+            <!-- <li><a class="dropdown-item" href="/esas/esas_admin/settings.php">Settings</a></li> -->
+            <li><a class="dropdown-item" href="/esas/esas_admin/history.php">History</a></li>
             <li><a class="dropdown-item" href="/esas/esas_admin/logout.php">Logout</a></li>
         </ul>
     </li>
