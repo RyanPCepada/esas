2f14170de0388c05d0c8fa48cea72929b9509319
RyanPCepada <ryancepada1992@gmail.com>
2024-10-14 11:09:24 +0800

    added activity logs for logout.php, admin folder

--- esas_admin/logout.php
+++ esas_admin/logout.php
@@ -1,9 +1,27 @@
 <?php
 // Initialize the session
 session_start();
+include '../config.php'; // Include your database connection
  
 // Set the default timezone to Asia/Manila
 date_default_timezone_set('Asia/Manila');
+
+// Check if the admin is logged in
+if (isset($_SESSION['admin_id'])) {
+    // Function to insert activity log
+    function insertActivityLog($pdo, $admin_id) {
+        $query = "INSERT INTO tbl_activity_logs (activity, dateAdded, admin_id) VALUES (:activity, :dateAdded, :admin_id)";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([
+            'activity' => 'You logged out of your account',
+            'dateAdded' => date('Y-m-d H:i:s'), // current timestamp
+            'admin_id' => $admin_id
+        ]);
+    }
+
+    // Insert the activity log for logout
+    insertActivityLog($pdo, $_SESSION['admin_id']);
+}
 
 // Unset all of the session variables
 $_SESSION = array();
