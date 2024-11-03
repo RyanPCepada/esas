017e7e4f27e247a0ba81e138a43e81fa4962e139
RyanPCepada <ryancepada1992@gmail.com>
2024-11-03 18:21:07 +0800

    bringing the config.php back to access our system

--- config.php
+++ config.php
@@ -3,10 +3,10 @@
 $host = 'localhost';
 $dbname = 'esas';
 $username = 'root';
-// $password =''; 
+$password =''; 
 
-$port =3307;
-$password ='root'; 
+// $port =3307;
+// $password ='root'; 
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 
@@ -16,8 +16,8 @@ define('DB_PASSWORD', '');
 define('DB_NAME', 'esas');
 
 try {
-     $pdo = new PDO("mysql:host=$host; port=$port; dbname=$dbname", $username, $password);
-    // $pdo = new PDO("mysql:host=$host;  dbname=$dbname", $username, $password);
+    // $pdo = new PDO("mysql:host=$host; port=$port; dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;  dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
