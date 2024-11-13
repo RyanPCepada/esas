7d0bb7939d417bb6928e4ce4e089acfccb65eda9
RyanPCepada <ryancepada1992@gmail.com>
2024-11-13 14:39:32 +0800

    removed also the student_id in add_officer_action.php

--- esas_moderator/actions/add_officer_action.php
+++ esas_moderator/actions/add_officer_action.php
@@ -13,9 +13,8 @@ $moderator_id = $_SESSION['moderator_id'];
 date_default_timezone_set('Asia/Manila');
 
 // Check if form data is submitted
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['position'], $_POST['student_id'], $_POST['club_id'])) {
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['position'], $_POST['club_id'])) {
     $position = $_POST['position'];
-    $studentId = $_POST['student_id'];
     $clubId = $_POST['club_id'];
 
     // Fetch the club name based on club_id
@@ -26,10 +25,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['position'], $_POST['st
     $clubName = $clubNameRow ? $clubNameRow['clubName'] : 'Unknown Club';
 
     // Insert new officer into the database
-    $insertSql = "INSERT INTO tbl_club_officers (club_id, student_id, position, dateAdded) VALUES (?, ?, ?, NOW())";
+    $insertSql = "INSERT INTO tbl_club_officers (club_id, position, dateAdded) VALUES (?, ?, NOW())";
     $insertStmt = $pdo->prepare($insertSql);
 
-    if ($insertStmt->execute([$clubId, $studentId, $position])) {
+    if ($insertStmt->execute([$clubId, $position])) {
         // Log the addition activity
         $logSql = "INSERT INTO tbl_activity_logs (activity, dateAdded, moderator_id) VALUES (:activity, :dateAdded, :moderator_id)";
         $logStmt = $pdo->prepare($logSql);
